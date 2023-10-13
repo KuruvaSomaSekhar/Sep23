2ec917c5c838ad024298b5eab2b81bbd4e96de0f
Soma Sekhar K <somasekhar.kuruva@gmail.com>
2023-10-13 07:42:13 +0530

    build pipeline 2

--- Jenkinsfile_build.groovy
+++ Jenkinsfile_build.groovy
@@ -6,7 +6,7 @@ pipeline{
         booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
         choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
 
-        string(name: 'source_Branch', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        string(name: 'source_Branch', defaultValue: 'master', description: 'Who should I say hello to?')
 
     }
     
@@ -31,7 +31,7 @@ pipeline{
         }
         stage("upload sep23artifacts to s3"){
             steps{
-                sh "aws s3 cp target/devops-*.war s3://sep23artifacts/$JOB_NAME/${params.source_Branch}/${BUILD_NUMBER}"
+                sh "aws s3 cp target/devops-*.war s3://sep23artifacts/$JOB_NAME/${params.source_Branch}/${BUILD_NUMBER}/"
             }
         }
 
